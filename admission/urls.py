59a31762515588cb02c158a05743aa8335e941a7
Kamoliddin Mirzaboyev <kamoliddinmirzaboyev2005@gmail.com>
2026-06-13 21:52:28 +0500

    fixxed

--- admission/urls.py
+++ admission/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import (
     AdmissionCurrentView, AdmissionHistoryView,
-    AdmissionSubjectsView, AdmissionSubjectDetailView,
-    AdmissionDocumentsView, AdmissionDocumentDetailView,
-    FAQListView, FAQFeaturedView, FAQDetailView,
-    DarsJadvaliListView, DarsJadvaliDetailView,
+    AdmissionSubjectListCreateView, AdmissionSubjectDetailView,
+    AdmissionDocumentListCreateView, AdmissionDocumentDetailView,
+    FAQListCreateView, FAQFeaturedView, FAQDetailView,
+    DarsJadvaliListCreateView, DarsJadvaliDetailView,
 )
 
 app_name = 'admission'
@@ -15,19 +15,19 @@ urlpatterns = [
     path('admission/history/', AdmissionHistoryView.as_view(), name='admission-history'),
 
     # Imtihon fanlari
-    path('admission/subjects/', AdmissionSubjectsView.as_view(), name='admission-subjects'),
+    path('admission/subjects/', AdmissionSubjectListCreateView.as_view(), name='admission-subjects'),
     path('admission/subjects/<int:pk>/', AdmissionSubjectDetailView.as_view(), name='admission-subject-detail'),
 
     # Talab qilinadigan hujjatlar
-    path('admission/documents/', AdmissionDocumentsView.as_view(), name='admission-documents'),
+    path('admission/documents/', AdmissionDocumentListCreateView.as_view(), name='admission-documents'),
     path('admission/documents/<int:pk>/', AdmissionDocumentDetailView.as_view(), name='admission-document-detail'),
 
     # FAQ lar
-    path('faqs/', FAQListView.as_view(), name='faq-list'),
+    path('faqs/', FAQListCreateView.as_view(), name='faq-list'),
     path('faqs/featured/', FAQFeaturedView.as_view(), name='faq-featured'),
     path('faqs/<int:pk>/', FAQDetailView.as_view(), name='faq-detail'),
 
     # Dars jadvali
-    path('dars-jadvali/', DarsJadvaliListView.as_view(), name='dars-jadvali-list'),
+    path('dars-jadvali/', DarsJadvaliListCreateView.as_view(), name='dars-jadvali-list'),
     path('dars-jadvali/<int:pk>/', DarsJadvaliDetailView.as_view(), name='dars-jadvali-detail'),
 ]
